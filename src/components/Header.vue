42cf2f55fef6b7a0078caace67b3494024770105
luoyong518 <471826078@qq.com>
2020-06-29 18:13:09 +0800

    header组件更新

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,15 @@
     <div class="hidden-md-and-up Header_brunb ">
       面包屑
     </div>
+  
     <div class="hidden-sm-and-down Header_container ">
+      <div class="Header_container_log">
+        <img :src="require('@/assets/images/vue.jpg')" alt="">
+        <div>
+           <p>关注前端开发</p>
+           <p>html5、Vue、React、Node</p>
+        </div>
+      </div>
       <el-menu
         :default-active="activeIndex"
         class="el-menu-demo"
@@ -12,9 +20,20 @@
         @select="handleSelect"
       >
         <el-menu-item index="1">首页</el-menu-item>
-        <el-menu-item index="2">选项1</el-menu-item>
-        <el-menu-item index="3">消息中心</el-menu-item>
-        <el-menu-item index="4">订单管理</el-menu-item>
+        <el-menu-item index="2">前端开发</el-menu-item>
+        <el-menu-item index="3">时光机</el-menu-item>
+        <el-menu-item index="4">作品连接</el-menu-item>
+        <!-- <el-menu-item index="5">生态</el-menu-item> -->
+         <el-submenu index="5">
+            <template slot="title">生态</template>
+            <el-menu-item index="2-1">Vue+Ts版本</el-menu-item>
+            <el-menu-item index="2-1">Vue3.0版本</el-menu-item>
+            <el-menu-item index="2-2">Vue+Nuxt.js版本</el-menu-item>
+            <el-menu-item index="2-3">React版本</el-menu-item>
+            <el-menu-item index="2-3">React+Next.js版本</el-menu-item>
+         </el-submenu>
+        <el-menu-item index="6">关于博客</el-menu-item>
+        <!-- <el-menu-item index="7">作品连接</el-menu-item> -->
       </el-menu>
     </div>
     <div class="Header_log">
@@ -60,7 +79,8 @@ export default class HelloWorld extends Vue {
 </script>
 <style lang="scss" scoped>
 .contanier{
-  background: linear-gradient(#fff,#d4d7db);
+  // background: linear-gradient(#fff,#d4d7db);
+  box-shadow: 3px 3px 5px #ada9a9;
 }
 .Header {
   position: relative;
@@ -76,7 +96,27 @@ export default class HelloWorld extends Vue {
     width: 900px;
     margin: 0 auto;
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
+    &_log{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      img{
+        height: 60px;
+      }
+      div{
+        display: flex;
+        flex-direction: column;
+        // align-items: center;
+        text-align: left;
+        p{
+          padding-left: 20px;
+          text-align: left;
+          line-height: 24px;
+          color: #bbb;
+        }
+      }
+    }
   }
   &_log {
     position: absolute;

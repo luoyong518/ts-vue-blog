c36028bb50660fc1139a9b31dacd3529745b7ee1
luoyong518 <471826078@qq.com>
2020-07-06 00:08:02 +0800

    1

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -32,10 +32,21 @@
     </div>
     <!-- 标签 -->
     <div class="labels">
-         <div class="labelsTitle sliderTitle">
+      <div class="labelsTitle sliderTitle">
         <label>热门标签</label>
-        <div>
-            
+      </div>
+      <div class="labelsBox">
+        <div v-for="(item,index) in linkList" :key="index" class="labelsBoxList">{{item.title}}</div>
+      </div>
+    </div>
+    <!-- 评论 -->
+    <div class="comment">
+      <div class="commentTitle sliderTitle">
+        <label>评论列表</label>
+      </div>
+      <div class="commentBox">
+        <div class="commentBoxList" v-for="(item,index) in commentList" :key="index">
+
         </div>
       </div>
     </div>
@@ -62,6 +73,17 @@ export default class HelloWorld extends Vue {
       lookCounts: 5
     }
   ];
+  private linkList: any[] = [
+    { id: 1, title: "百度", url: "http://www.baidu.com" },
+    { id: 2, title: "百度", url: "http://www.baidu.com" },
+    { id: 3, title: "百度", url: "http://www.baidu.com" },
+    { id: 4, title: "百度", url: "http://www.baidu.com" },
+    { id: 5, title: "百度", url: "http://www.baidu.com" },
+    { id: 6, title: "百度", url: "http://www.baidu.com" }
+  ];
+  private commentList: any[] = [
+    { id: 1, }
+  ]
 }
 </script>
 <style lang="scss" scoped>
@@ -71,21 +93,20 @@ export default class HelloWorld extends Vue {
   box-shadow: 3px 3px 3px #ccc;
   border-radius: 4px;
 
- .sliderTitle {
-      border-bottom: 1px solid #eee;
-      label {
-        display: inline-block;
-        line-height: 44px;
-        height: 44px;
-        border-bottom: 2px solid aqua;
-      }
+  .sliderTitle {
+    border-bottom: 1px solid #eee;
+    label {
+      display: inline-block;
+      line-height: 44px;
+      height: 44px;
+      border-bottom: 2px solid aqua;
     }
+  }
   //跟新列表
   .updateList {
     margin-top: 15px;
     padding-bottom: 15px;
     background-color: #fff;
-   
 
     &Box {
       padding: 0 15px;
@@ -95,8 +116,8 @@ export default class HelloWorld extends Vue {
       display: flex;
       justify-content: space-between;
       border-bottom: 1px solid #eee;
-      &:last-child{
-          border-color: transparent;
+      &:last-child {
+        border-color: transparent;
       }
       &Img {
         width: 105px;
@@ -113,7 +134,7 @@ export default class HelloWorld extends Vue {
           font-size: 14px;
         }
         &Likes {
-            color: #999;
+          color: #999;
           height: 22px;
           display: flex;
           justify-content: space-between;
@@ -123,10 +144,35 @@ export default class HelloWorld extends Vue {
       }
     }
   }
-  .labels{
+  .labels {
+    margin-top: 15px;
+    // padding-bottom: 15px;
+    background-color: #fff;
+    &Box {
+      padding: 10px 15px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      &List {
+        // flex: 1;
+        width: 90px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        margin-bottom: 10px;
+        background-color: #f6f6f6;
+        color: #999;
+        &:hover {
+          background-color: aqua;
+          color: #fff;
+        }
+      }
+    }
+  }
+  // 
+  .comment{
      margin-top: 15px;
-    padding-bottom: 15px;
-    background-color: #fff;  
+    background-color: #fff;
   }
 }
 </style>
